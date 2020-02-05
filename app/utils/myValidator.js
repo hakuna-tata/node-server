96cc279b35f6ae45c549f0d217bd7093b77ed3ad
hakuna-tata <610578197@qq.com>
2020-02-05 22:51:14 +0800

    参数校验器基础功能完成

--- app/utils/myValidator.js
+++ app/utils/myValidator.js
@@ -1,10 +1,4 @@
 const validator = require("validator")
-const {
-  get,
-  last,
-  set,
-  cloneDeep
-} = require("lodash")
 
 class MyValidator{
   constructor(){
@@ -24,13 +18,13 @@ class MyValidator{
   _getParamValue(key){
     let value
     // 1.没有处理对象键值嵌套对象或者数组等参数类型
-    // 2.没有处理可选参数传递默认值
     for(let element in this.data){
       if(this.data[element][key]){
         value = this.data[element][key]
-        return value
+        break
       }
     }
+    return value
   }
 
   _getDefinedRule(originObj){
@@ -44,8 +38,12 @@ class MyValidator{
     }
   }
 
-  _check(value,rule){
-
+  _checkRules(value,rules){
+    let ruleResult = {}
+    rules.forEach(rule => {
+      ruleResult = rule.validate(value)
+    })
+    console.log(ruleResult)
   }
   
   validate(ctx){
@@ -54,8 +52,16 @@ class MyValidator{
     this._getDefinedRule.call(this,this)
     for(let key in this.definedRule){
       let value = this._getParamValue(key)
-      this._check(value,this.definedRule[key])
+      this._checkRules(value,this.definedRule[key])
     }
+  }
+}
+
+class RuleResult{
+  constructor(pass = false, message, value = null){
+    Object.assign(this,{
+      pass, message, value
+    })
   }
 }
 
@@ -64,14 +70,29 @@ class Rule{
     Object.assign(this,{
       validateFunction, message, options
     })
-    if(this.validateFunction === "isOptional"){
-      this.optional = true
-      this.defaultValue = options && options[0]
-    }
   }
 
-  validate(context){
-
+  validate(value){
+    if(!value){
+      // 自定义可选参数
+      if(this.validateFunction === "isOptional"){
+        let defaultValue = this.options && this.options[0]
+        return new RuleResult(true, "", defaultValue)
+      }else{
+        return new RuleResult(false, "字段是必传参数")
+      }
+    }
+    // validator不存在的api
+    if(!validator[this.validateFunction]){
+      let msg = `${this.validateFunction}不存在`
+      return new RuleResult(false, msg)
+    }
+    // 自定义函数
+    else if(typeof this.validateFunction === "function"){
+      return this.validateFunction(value,...this.options)
+    }
+    let result = validator[this.validateFunction](value,...this.options)
+    return new RuleResult(result, result ? "" : "参数错误", value)
   }
 }
 
--- app/validator.js
+++ app/validator.js
@@ -4,16 +4,22 @@ class RegisterValidator extends MyValidator{
   constructor(){
     super()
     this.username= [
-      new Rule()
+      new Rule("isLength", "昵称不符合长度规范", {
+        min: 4,
+        max: 32
+      }),
     ]
     this.email= [
-      new Rule()
+      new Rule("isEmail", "电子邮箱不符合规范,请输入正确的邮箱")
     ]
-    this.password1 = [
-      new Rule(),
-      new Rule()
+    this.age = [
+      new Rule("isOptional")
     ]
-    this.password2 = this.password1
+    // this.password1 = [
+    //   new Rule(),
+    //   new Rule()
+    // ]
+    // this.password2 = this.password1
   }
 
   validatePassword(){
